8e5f2e61594a1257ce6262f632fc43cde7ef9987
rkddntjd7 <125332196+rkddntjd7@users.noreply.github.com>
2023-09-16 14:48:49 +0900

    주문 서비스 개발 까지

--- 스프링 부트와 JPA 활용1/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ 스프링 부트와 JPA 활용1/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -26,4 +27,24 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //비즈니스 로직
+
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int resetStock = this.stockQuantity - quantity;
+        if (resetStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = resetStock;
+    }
 }
